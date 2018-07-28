fc644559bc6a16bf80c134c6a827549fd89dc11f
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-07-28 16:37:07 -0700

    Change Login.js from js to jsx

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Button, Intent } from "@blueprintjs/core";
 
 const Login = () => (
-    <div>
-        <h2>Login</h2>
-        {React.createElement(Button, { intent: Intent.SUCCESS }, "Sign In")}
-    </div>
+  <div>
+    <h2>Login</h2>
+    <Button>Sign In</Button>
+  </div>
 )
 
 export default Login
