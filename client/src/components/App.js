ceed206eea7212ecf6cc07b0ed28292b91f77466
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-07-28 17:38:01 -0700

    gitignore build files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,8 +3,9 @@ import {
   BrowserRouter as Router,
   Route,
   Link,
-  Tooltip
+  withRouter
 } from 'react-router-dom'
+import { Navbar, Button, Card, Elevation } from '@blueprintjs/core';
 
 import Login from './Login';
 import ControlPanel from './ControlPanel';
@@ -23,17 +24,18 @@ const App = () => (
         <li><Link to="/createProperties">Create Properties</Link></li>
         <li><Link to="/calendar">Calendar</Link></li>
         <li><Link to="/calendars">Calendars</Link></li>
-        
+
       </ul>
 
-      <hr/>
-
-      <Route exact path="/" component={Login} large={true}/>
-      <Route path="/controlPanel" component={ControlPanel}/>
-      <Route path="/createManagers" component={CreateManagers}/>
-      <Route path="/createProperties" component={CreateProperties}/>
-      <Route path="/calendar" component={Calendar}/>
-      <Route path="/calendars" component={Calendars}/>
+      <hr />
+      <Card interactive={true} elevation={Elevation.TWO}>
+        <Route exact path="/" component={Login} large={true} />
+        <Route path="/controlPanel" component={ControlPanel} />
+        <Route path="/createManagers" component={CreateManagers} />
+        <Route path="/createProperties" component={CreateProperties} />
+        <Route path="/calendar" component={Calendar} />
+        <Route path="/calendars" component={Calendars} />
+      </Card>
     </div>
   </Router>
 )
