7ee1811f455b73c069817dd62e837e37d831103c
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-08-01 19:32:23 -0700

    change theme, add home icon to nav

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,25 +12,16 @@ import CreateManagers from './CreateManagers';
 import CreateProperties from './CreateProperties';
 import Calendar from './Calendar';
 import Calendars from './Calendars';
-import Layout from './Layout';
+import Home from './Home';
 
 const App = () => (
   <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Login</Link></li>
-        <li><Link to="/controlPanel">Control Panel</Link></li>
-        <li><Link to="/createManagers">Create Managers</Link></li>
-        <li><Link to="/createProperties">Create Properties</Link></li>
-        <li><Link to="/calendar">Calendar</Link></li>
-        <li><Link to="/calendars">Calendars</Link></li>
-
-      </ul>
-
-
+    <div className="bp3-dark">
+      
       <hr />
       <div>
-        <Route exact path="/" component={Login} large={true} />
+        <Route exact path='/' component={Home} />
+        <Route exact path="/login" component={Login} large={true} />
         <Route path="/controlPanel" component={ControlPanel} />
         <Route path="/createManagers" component={CreateManagers} />
         <Route path="/createProperties" component={CreateProperties} />
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Card, Elevation } from '@blueprintjs/core';
-
+import { Card, Elevation, Navbar, Button, Alignment } from '@blueprintjs/core';
+import { Link } from 'react-router-dom';
 
 const Layout = props =>
-  <Card interactive={true} elevation={Elevation.TWO}>
-    {props.children}
- </Card>
+  <div>
+    <Navbar fixedToTop>
+      <Navbar.Group align={Alignment.LEFT}>
+        <Navbar.Heading>Turbo-Telegram</Navbar.Heading>
+        <Navbar.Divider />
+        <Link to="/">
+        <Button className="bp3-minimal" icon="home" text="Home"/>
+        </Link>
+        <Link to="/login">
+        </Link>
+        <Link to="/controlPanel">Control Panel</Link>
+        <Link to="/createManagers">Create Managers</Link>
+        <Link to="/createProperties">Create Properties</Link>
+        <Link to="/calendar">Calendar</Link>
+        <Link to="/calendars">Calendars</Link>
+
+      </Navbar.Group>
+    </Navbar>
+    <Card interactive={true} elevation={Elevation.TWO}>
+      {props.children}
+    </Card>
+  </div>
 
 export default Layout;
