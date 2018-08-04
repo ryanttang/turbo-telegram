cce60dba8635937695445bee3c4190e49a453aa7
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-08-04 10:46:42 -0700

    add rafa's login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Button, Intent } from "@blueprintjs/core";
+import { Button, Intent, InputGroup } from "@blueprintjs/core";
 import Layout from './Layout'
 
 const Login = () => (
   <div>
     <h2>Login</h2>
+    <h4>Username:<InputGroup 
+      leftIcon="tag"
+      placeholder="Please enter your username..."/></h4>
+    <h4>Password:<InputGroup 
+      leftIcon="lock"
+      placeholder="Please enter your password..."/></h4>
     <Button>Sign In</Button>
   </div>
 )
