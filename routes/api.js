93b90f3a823dc59dd0161946ed680c108e141bf2
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-08-06 00:07:25 -0700

    add route api/login with user bobby pswd dallas, that returns true if correct password

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const Login = require('./models/Login');
+const Login = require('../models/Login');
 
 const { Lead, Manager, Property, Worker } = require('../models');
 const c = (x, i) => c ? cl.x45.bold.underline(i) : cl.green.xb34.underline(i);
@@ -19,8 +19,11 @@ router.post('/createuser', function(req, res, next) {
 
 router.post('/login', function(req, res, next) {
   const { username, password } = req.body;
-    Login.find({username}, (err, name) => {
-      
+    Login.find({username}, (err, user) => {
+      if(err) throw err;
+      password === user[0].password ?
+        res.send(true)
+        : res.json(false);
     })
   
 })
