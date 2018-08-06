0a728b713c0e3335189e4b0e2d83c24af18f5e02
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-08-05 22:44:00 -0700

    separate loginModal component from Layout

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,20 +1,17 @@
 import React, { PureComponent } from 'react';
-import { Card, InputGroup, Elevation, Navbar, Button, Alignment } from '@blueprintjs/core';
-import { AnchorButton, Classes, Code, Dialog, H5, Intent, Switch, Tooltip } from "@blueprintjs/core";
-import { Example, handleBooleanChange, IExampleProps } from "@blueprintjs/docs-theme";
-import { Link, Route } from 'react-router-dom';
-// import Dashboard from 'react-dazzle';
+import { Card, Elevation, Navbar, Button, Alignment } from '@blueprintjs/core';
+import { Link } from 'react-router-dom';
 import Sidemenu from './Sidemenu';
+import LoginModal from './LoginModal';
 
 
 class Layout extends PureComponent {
-  state = {show: false}
-  showModal = () => {
-    this.setState({ isOpen: true });
+  state = {
+    showModal: false
   }
   
-  hideModal = () => {
-    this.setState({ isOpen: false });
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
   }
 
   render() {
@@ -29,37 +26,10 @@ class Layout extends PureComponent {
               <Button className="bp3-minimal" icon="home" text="Home" />
             </Link>
             {/* Link goes nowhere but for continuity I wrapped the Button in a Link tag so it will appear blue just like the Home button.  */}
-            <Link to>
-              <Button  className="bp3-minimal"  onClick={this.showModal} icon="person" text="Login" />
+            <Link to="">
+              <Button className="bp3-minimal" onClick={this.toggleModal} icon="person" text="Login" />
               </Link>
-              <Dialog
-                className="bp3-dark"
-                icon="info-sign"
-                onClose={this.hideModal}
-                title="Turbo-Telegram"
-                {...this.state} >
-           
-                <div className={Classes.DIALOG_BODY}>
-                <div>
-                  <h2>Login</h2>
-                  <h4>Username:<InputGroup 
-                    leftIcon="tag"
-                    placeholder="Please enter your username..."/></h4>
-                  <h4>Password:<InputGroup 
-                    leftIcon="lock"
-                    placeholder="Please enter your password..."/></h4>
-                  <Button>Sign In</Button>
-                </div>
-              </div>
-                <div className={Classes.DIALOG_FOOTER}>
-                    <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-                        <Tooltip content="This button is hooked up to close the dialog.">
-                            <Button onClick={this.hideModal}>Close</Button>
-                        </Tooltip>
-                       
-                    </div>
-                </div>
-            </Dialog>
+              <LoginModal show={this.state.showModal} toggleModal={this.toggleModal}/>
           </Navbar.Group>
         </Navbar>
         <div className="grid">
