c846f170862895db365b4eed4abc11c8dda6a663
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-08-05 22:54:11 -0700

    Add log out

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -7,11 +7,16 @@ import LoginModal from './LoginModal';
 
 class Layout extends PureComponent {
   state = {
-    showModal: false
+    showModal: false,
+    loggedIn: true
   }
   
   toggleModal = () => {
     this.setState({ showModal: !this.state.showModal });
+  }
+
+  logOut = () => {
+    this.setState({loggedIn: false })
   }
 
   render() {
@@ -26,9 +31,15 @@ class Layout extends PureComponent {
               <Button className="bp3-minimal" icon="home" text="Home" />
             </Link>
             {/* Link goes nowhere but for continuity I wrapped the Button in a Link tag so it will appear blue just like the Home button.  */}
-            <Link to="">
+            { 
+              this.state.loggedIn ?
+              <Link to="">
+              <Button className="bp3-minimal" onClick={this.logOut} icon="person" text="log-out" />
+            </Link>
+            : <Link to="">
               <Button className="bp3-minimal" onClick={this.toggleModal} icon="person" text="Login" />
-              </Link>
+            </Link>
+            }
               <LoginModal show={this.state.showModal} toggleModal={this.toggleModal}/>
           </Navbar.Group>
         </Navbar>
