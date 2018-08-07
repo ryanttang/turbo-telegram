7f08ade077538dd79a88fd9489d3e73a1b4ac8c3
Rafael <rafraga@gmail.com>
2018-08-06 20:10:46 -0700

    layout changes

--- client/src/components/CreateProperties.js
+++ client/src/components/CreateProperties.js
@@ -3,47 +3,53 @@ import Layout from './Layout';
 import { Button, Intent, InputGroup, Dialog, FileInput, MenuItem, Menu} from "@blueprintjs/core";
 
   const CreateProperties = () => (
-  <div>
-  <h1>Create Properties</h1>
-  <div class="grid-container1">
-  <h4>Property Name:<InputGroup className="grid-item1"
-  leftIcon="home"
-  placeholder="Please enter property name..."/></h4>
-  <h4>Property Phone:<InputGroup className="grid-item1"
-  leftIcon="phone"
-  placeholder="Please enter property phone number..."/></h4>
-  </div>
+    <div>
+      <div class="grid-container1">
+       <div>
+        <br></br>
+        <h1>Create Property</h1>
+        <h4>Property Name:<InputGroup className="grid-item"
+          leftIcon="home"
+          placeholder="Please enter property name..."/></h4>
+           <h4>Amount of Units:<InputGroup className="grid-item"
+          leftIcon="home"
+          placeholder="Please enter total amount of units..."/></h4>
+        </div>
+        <div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <h4>Property Phone:<InputGroup className="grid-item"
+            leftIcon="phone"
+            placeholder="Please enter property phone number..."/></h4>
+          <h4>Property Manager:<Menu className="grid-item">
+            <MenuItem
+              icon="person"
+              rightIcon="caret-down"
+              text="Assign a manager to this property" >
+                <MenuItem text="Manager 1" />
+                <MenuItem text="Manager 2" />
+                <MenuItem text="Manager 3" />
+            </MenuItem>
+        </Menu></h4>
 
-  <div class="grid-container1">
-  <h4>Amount of Units:<InputGroup className="grid-item1"
-  leftIcon="home"
-  placeholder="Please enter total amount of units..."/></h4>
-  <h4>Assign Manager:<Menu className="grid-item1">
-    <MenuItem text="Please assign a manager to this property" 
-      icon="numbered-list"
-      rightIcon="caret-down"
-      text="Please assign a manager to this property" >
-        <MenuItem text="Manager 1" />
-        <MenuItem text="Manager 2" />
-        <MenuItem text="Manager 3" />
-    </MenuItem>
-</Menu>
-                        </h4>
-  </div>
-
-
-  <h4>Address:<InputGroup className="grid-item2"
-  leftIcon="geolocation"
-  placeholder="Please enter property address..."/></h4>
-  <h4>Address 2:<InputGroup className="grid-item2"
-  leftIcon="geolocation"
-  placeholder="Please enter property address..."/></h4>  
-  <Button>Create</Button>
-  <div class="person">
-  <img src="./property.png" width="300" />
-  <center><FileInput disabled={false} text="Choose file..." /></center>
-  </div>
-  </div>
+        </div>
+          <div class="image">
+            <h4><img src="./property.png" width="180" height="180" />
+            <FileInput id="loadimage" disabled={false} text="Choose file..." /></h4>
+          </div>
+      </div>
+      <div>
+        <h4>Address:<InputGroup className="grid-item2"
+          leftIcon="geolocation"
+          placeholder="Please enter manager address..."/></h4>
+        <h4>Address 2:<InputGroup className="grid-item2"
+          leftIcon="geolocation"
+          placeholder="Please enter manager address..."/></h4>  
+        <Button>Create</Button>
+      </div>
+      </div>
   )
 
 export default CreateProperties;
