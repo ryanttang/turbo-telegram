8623e46feb0377e28fafe978e657d68d6d14ab38
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-08-10 22:04:57 -0700

    add mongodb_uri for deployment in seed

--- utils/seed.js
+++ utils/seed.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const User = require('../models/User');
 
 
-mongoose.connect('mongodb://localhost:27017/turbodb', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/turbodb', {
   useNewUrlParser: true
 });
 
